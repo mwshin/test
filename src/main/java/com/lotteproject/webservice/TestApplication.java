656399649f4ac6365638a6bb75478736901787a7
lotte_admin_it <admin_it@lotteshopping.com>
2019-12-31 13:52:37 +0900

    no message

--- src/main/java/com/lotteproject/webservice/TestApplication.java
+++ src/main/java/com/lotteproject/webservice/TestApplication.java
@@ -3,11 +3,14 @@ package com.lotteproject.webservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.lotteproject.webservice.test.MarshalExample;
+
 @SpringBootApplication
 public class TestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
+		new MarshalExample().marshalTest();
 	}
 
 }
